98cddb526d04f2ec1a662d82ab681719e20dddd2
Kefas Kingsley <Kefasiceking@gmail.com>
2021-01-15 22:04:55 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,7 +47,7 @@ function handleImage(e) {
         img.onload = function() {
             const canvas = document.getElementById("canvas");
             const ctx = canvas.getContext("2d");
-            ctx.drawImage(img, 715, 180, 333, 470);
+            ctx.drawImage(img, 715, 180, 310, 450);
         };
         img.src = event.target.result;
     };
